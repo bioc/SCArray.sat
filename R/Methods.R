86a88e1fe329fbc0f63e9bdb349064f2164577c4
Xiuwen Zheng <zhengxwen@users.noreply.github.com>
2023-01-14 22:31:07 -0600

    add new scale.data2

--- R/Methods.R
+++ R/Methods.R
@@ -16,8 +16,7 @@
 .redirect_slot <- function(nm)
 {
     stopifnot(is.character(nm), length(nm)==1L)
-    # switch(nm, data="data2", counts="counts2", scale.data="scale.data2", nm)
-    switch(nm, data="data2", counts="counts2", nm)
+    switch(nm, data="data2", counts="counts2", scale.data="scale.data2", nm)
 }
 
 
@@ -30,78 +29,71 @@ GetAssayData.SCArrayAssay <- function(object,
 {
     CheckDots(...)
     slot <- match.arg(slot)
-    # output
-    slot(object, .redirect_slot(slot))
+    slot(object, .redirect_slot(slot))  # output
 }
 
 
 SetAssayData.SCArrayAssay <- function(object,
     slot=c('data', 'scale.data', 'counts'), new.data, ...)
 {
-  CheckDots(...)
-  # print(dim(object))
-  # print(dim(new.data))
-  slot <- slot[1]
-  slot <- match.arg(arg = slot)
-  if (!IsMatrixEmpty(x = new.data)) {
-    if (ncol(x = new.data) != ncol(x = object)) {
-      stop(
-        "The new data doesn't have the same number of cells as the current data",
-        call. = FALSE
-      )
+    # check
+    CheckDots(...)
+    slot <- match.arg(slot)
+    if (!IsMatrixEmpty(new.data))
+    {
+        if (any(grepl('_', rownames(new.data), fixed=TRUE)))
+        {
+            warning("Feature names cannot have underscores ('_'), replacing with dashes ('-')",
+                call.=FALSE, immediate.=TRUE)
+            rownames(new.data) <- gsub('_', '-', rownames(new.data), fixed=TRUE)
+        }
+        if (ncol(new.data) != ncol(object))
+        {
+            stop("The new data doesn't have the same number of cells as the current data",
+                call.=FALSE)
+        }
+        num.counts <- nrow(object)
+        counts.names <- rownames(object)
+        if (slot=='scale.data' && nrow(new.data)>num.counts)
+        {
+            warning("Adding more features than present in current data",
+                call.=FALSE, immediate.=TRUE)
+        } else if (slot %in% c('counts', 'data') && nrow(new.data)!=num.counts)
+        {
+            warning("The new data doesn't have the same number of features as the current data",
+                call.=FALSE, immediate.=TRUE)
+        }
+        if (!all(rownames(new.data) %in% counts.names))
+        {
+            warning("Adding features not currently present in the object",
+            call.=FALSE, immediate.=TRUE)
+        }
+        new.features <- na.omit(match(counts.names, rownames(new.data)))
+        new.cells <- colnames(new.data)
+        if (!all(new.cells %in% colnames(object)))
+        {
+            stop("All cell names must match current cell names", call.=FALSE)
+        }
+        new.data <- new.data[new.features, colnames(object), drop=FALSE]
+        if (slot %in% c('counts', 'data') && !all(dim(new.data)==dim(object)))
+        {
+            stop("Attempting to add a different number of cells and/or features",
+                call.=FALSE)
+        }
     }
-    num.counts <- nrow(x = object)
-    counts.names <- rownames(x = object)
-    if (slot == 'scale.data' && nrow(x = new.data) > num.counts) {
-      warning(
-        "Adding more features than present in current data",
-        call. = FALSE,
-        immediate. = TRUE
-      )
-    } else if (slot %in% c('counts', 'data') && nrow(x = new.data) != num.counts) {
-      warning(
-        "The new data doesn't have the same number of features as the current data",
-        call. = FALSE,
-        immediate. = TRUE
-      )
+    if (!is.vector(rownames(new.data)))
+    {
+        rownames(new.data) <- as.vector(rownames(new.data))
     }
-    if (!all(rownames(x = new.data) %in% counts.names)) {
-      warning(
-        "Adding features not currently present in the object",
-        call. = FALSE,
-        immediate. = TRUE
-      )
+    if (!is.vector(colnames(new.data)))
+    {
+        colnames(new.data) <- as.vector(colnames(new.data))
     }
-    new.features <- na.omit(object = match(
-      x = counts.names,
-      table = rownames(x = new.data)
-    ))
-    new.cells <- colnames(x = new.data)
-    if (!all(new.cells %in% colnames(x = object))) {
-      stop(
-        "All cell names must match current cell names",
-        call. = FALSE
-      )
-    }
-    new.data <- new.data[new.features, colnames(object), drop = FALSE]
-    # print(dim(new.data))
-    # print(dim(object))
-    if (slot %in% c('counts', 'data') && !all(dim(new.data) == dim(object))) {
-      stop(
-        "Attempting to add a different number of cells and/or features",
-        call. = FALSE
-      )
-    }
-  }
-  if (!is.vector(x = rownames(x = new.data))) {
-    rownames(x = new.data) <- as.vector(x = rownames(x = new.data))
-  }
-  if (!is.vector(x = colnames(x = new.data))) {
-    colnames(x = new.data) <- as.vector(x = colnames(x = new.data))
-  }
-  slot(object = object, name = .redirect_slot(slot)) <- new.data
-  return(object)
+    # set
+    slot(object, .redirect_slot(slot)) <- new.data
+    return(object)
 }
+
 
 
 ####  Methods -- CreateSeuratObject()  ####
